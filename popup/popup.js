61bc95d3e5357b2dc5b36cf6d47ce23013948abb
PrashanthaTP <tpprashantha@gmail.com>
2023-07-22 23:05:08 +0530

    Feat(#2): Implement user input and basic list reordering

--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,53 @@
-import "../lib/fuzzy.js"
+import { getDistance } from "../lib/fuzzy.js"
+
+const queryForm = document.querySelector("#query-form")
+const queryInput = document.querySelector("#query-input")
 
 const templateListItem = document.querySelector("#template-li")
 const stringsUl = document.querySelector("#strings-ul")
-let listItems = ["aytala","macha"]
+
+let listItems = ["aytala","macha","sakkath","kana","aramagiru","cool","machi","super"]
+
 const getListItem = (string) => {
     const element = templateListItem.content.firstElementChild.cloneNode(true);
     element.textContent = string
     return element
 }
 
-const main = () => {
 
+const init = () => {
+    for(let string of listItems){
+        stringsUl.appendChild(getListItem(string))
+    }
+
+}
+const main = (query) => {
+    stringsUl.innerHTML = ""
+    let scores = new Map()
+    for(let item of listItems){
+        scores[item] = getDistance(query,item)
+    }
+    listItems.sort((a,b)=>{
+        if(scores[a]>=scores[b]){
+            // a is more near to query
+            return -1;
+        }else{
+            return 1;
+        }
+    })
     for(let string of listItems){
         stringsUl.appendChild(getListItem(string))
     }
 }
-main()
+
+queryInput.addEventListener('input',(e)=>{
+    e.preventDefault()
+    main(queryInput.value)
+})
+queryForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    main(queryInput.value)
+})
 
 
+init()
