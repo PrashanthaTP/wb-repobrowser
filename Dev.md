c7a0a0b5fc13ae4b3c0758b8fe5ee04f27a11b96
PrashanthaTP <tpprashantha@gmail.com>
2023-07-22 23:20:23 +0530

    Feat(#3): Implment Debounce/Throttole

---
+++ Dev.md
@@ -0,0 +1,7 @@
+# Dev
+
+## Arrays
+
++ [Stack overflow : 2D arrays](https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript)
++ [Stack overflow : input changes](https://stackoverflow.com/questions/4886114/how-can-i-run-a-javascript-function-when-input-changes)
++ [Stack overflow : Debouncing and Throttling](https://stackoverflow.com/questions/25991367/difference-between-throttling-and-debouncing-a-function)
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
 import { getDistance } from "../lib/fuzzy.js"
+import { debounce } from "../lib/polyfill.js"
 
 const queryForm = document.querySelector("#query-form")
 const queryInput = document.querySelector("#query-input")
@@ -6,7 +7,7 @@ const queryInput = document.querySelector("#query-input")
 const templateListItem = document.querySelector("#template-li")
 const stringsUl = document.querySelector("#strings-ul")
 
-let listItems = ["aytala","macha","sakkath","kana","aramagiru","cool","machi","super"]
+let listItems = ["aytala", "macha", "sakkath", "kana", "aramagiru", "cool", "machi", "super"]
 
 const getListItem = (string) => {
     const element = templateListItem.content.firstElementChild.cloneNode(true);
@@ -16,35 +17,35 @@ const getListItem = (string) => {
 
 
 const init = () => {
-    for(let string of listItems){
+    for (let string of listItems) {
         stringsUl.appendChild(getListItem(string))
     }
 
 }
-const main = (query) => {
+const main = debounce((query) => {
     stringsUl.innerHTML = ""
     let scores = new Map()
-    for(let item of listItems){
-        scores[item] = getDistance(query,item)
+    for (let item of listItems) {
+        scores[item] = getDistance(query, item)
     }
-    listItems.sort((a,b)=>{
-        if(scores[a]>=scores[b]){
+    listItems.sort((a, b) => {
+        if (scores[a] >= scores[b]) {
             // a is more near to query
             return -1;
-        }else{
+        } else {
             return 1;
         }
     })
-    for(let string of listItems){
+    for (let string of listItems) {
         stringsUl.appendChild(getListItem(string))
     }
-}
+},500);
 
-queryInput.addEventListener('input',(e)=>{
+queryInput.addEventListener('input', (e) => {
     e.preventDefault()
     main(queryInput.value)
 })
-queryForm.addEventListener('submit',(e)=>{
+queryForm.addEventListener('submit', (e) => {
     e.preventDefault()
     main(queryInput.value)
 })
